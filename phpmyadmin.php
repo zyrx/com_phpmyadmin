cd3c05de0b6727421637d5fe3d240b3ffe61d0c2
Lech H. Conde <lech@mavels.net>
2015-07-09 13:00:38 -0500

    Added initial files

---
+++ phpmyadmin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package      Joomla.Administrator
+ * @subpackage   com_phpmyadmin
+ * @author       Lech H. Conde <lech@mavels.net>
+ * @copyright    Copyright (C) 2015 Open Source Matters. All rights reserved.
+ * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die;
